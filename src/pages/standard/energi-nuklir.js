1c4f065b2458b4962e0f216142bfa89cd4172373
Sevalino Elfata <sevalinoelfata8@gmail.com>
2024-10-08 20:33:57 +0700

    standar

--- src/pages/standard/energi-nuklir.js
+++ src/pages/standard/energi-nuklir.js
@@ -5,28 +5,89 @@ const EnergiNuklirStandard = () => {
     const data = [
         {
             no: 1,
-            item: 'SNI Nuklir 1',
-            nama: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium feugiat ex quis auctor. Fusce vel rutrum orci.',
-            link: 'https://example.com',
+            item: 'SNI ISO 12749-5:2018',
+            nama: 'Energi nuklir, teknologi nuklir dan proteksi radiologi - Kosakata - Bagian 5 : Reaktor Nuklir (ISO 12749-5:2018, IDT)',
+            link: 'https://pesta.bsn.go.id/produk/detail/13858-sniiso12749-52018',
             description: [],
         },
         {
             no: 2,
-            item: 'SNI Nuklir 2',
-            nama: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium feugiat ex quis auctor. Fusce vel rutrum orci.',
-            link: 'https://example.com',
+            item: 'SNI 04-6265.1-2000',
+            nama: 'Instrumentasi nuklir - Sistem pengukuran dan instrumen listrik yang menggunakan sumber radiasi pengionan - Aspek umum',
+            link: 'https://pesta.bsn.go.id/produk/detail/5796-sni04-62651-2000',
             description: [],
         },
         {
             no: 3,
-            item: 'SNI Nuklir 3',
-            nama: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium feugiat ex quis auctor. Fusce vel rutrum orci.',
-            link: null,
-            description: [
-                'https://example1.com',
-                'https://example2.com',
-            ],
+            item: 'SNI 04-6266-2000',
+            nama: 'Pembangkit listrik tenaga nuklir - Ruang kendali utama - Verifikasi dan perlakuan desain',
+            link: 'https://pesta.bsn.go.id/produk/detail/5798-sni04-6266-2000',
+            description: [],
         },
+        {
+            no: 4,
+            item: 'SNI IEC 60780:2008',
+            nama: 'Pembangkit Listrik Tenaga Nuklir - Peralatan elektris sistem keselamatan – Kualifikasi',
+            link: 'https://pesta.bsn.go.id/produk/detail/7611-sniiec607802008',
+            description: [],
+        },
+        {
+            no: 5,
+            item: 'SNI IEC 60231D:1975',
+            nama: 'Prinsip umum instrumentasi reaktor nuklir - Prinsip instrumentasi untuk reaktor air bertekanan (IEC 60231D: 1975, IDT)',
+            link: 'https://pesta.bsn.go.id/produk/detail/13609-sniiec60231d1975',
+            description: [],
+        },
+        {
+            no: 6,
+            item: 'SNI 04-6266-2000',
+            nama: 'Pembangkit listrik tenaga nuklir - Ruang kendali utama - Verifikasi dan perlakuan desain',
+            link: 'https://pesta.bsn.go.id/produk/detail/5798-sni04-6266-2000',
+            description: [],
+        },
+        {
+            no: 7,
+            item: 'SNI 18-2034-1990',
+            nama: 'Pedoman penentuan tapak reaktor nuklir',
+            link: null,
+            description: [],
+        },
+        {
+            no: 8,
+            item: 'SNI 18-4149-1996',
+            nama: 'Kualifikasi personel untuk operasi dan pemeliharaan reaktor nuklir',
+            link: null,
+            description: [],
+        },
+        {
+            no: 9,
+            item: 'SNI 18-7103-2005',
+            nama: 'Pembangkit listrik tenaga nuklir - Sistem instrumentasi dan kendali yang penting untuk keselamatan - Klasifikasi',
+            link: 'https://pesta.bsn.go.id/produk/detail/6871-sni18-7103-2005',
+            description: [],
+        },
+        {
+            no: 10,
+            item: 'SNI 8873:2019',
+            nama: 'Standar pengujian material grafit untuk komponen reaktor nuklir berpendingin gas (ASTM C781-18, IDT)',
+            link: 'https://pesta.bsn.go.id/produk/detail/12515-sni88732019',
+            description: [],
+        },
+        {
+            no: 11,
+            item: 'SNI IEC 60231D:1975',
+            nama: 'Prinsip umum instrumentasi reaktor nuklir - Prinsip instrumentasi untuk reaktor air bertekanan (IEC 60231D: 1975, IDT)',
+            link: 'https://pesta.bsn.go.id/produk/detail/13609-sniiec60231d1975',
+            description: [],
+        },
+        {
+            no: 12,
+            item: 'SNI IEC 60557:2009',
+            nama: 'Persitilahan instrumentasi dalam bidang reaktor nuklir',
+            link: 'https://pesta.bsn.go.id/produk/detail/7941-sniiec605572009',
+            description: [],
+        }
+        
     ];
 
     return (
